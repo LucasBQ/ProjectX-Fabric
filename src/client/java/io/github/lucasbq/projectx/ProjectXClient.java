cf6521005297dcaff786c8c759e7608871bff85e
LucasBQ <10766315+LucasBQ@users.noreply.github.com>
2024-08-10 18:29:33 -0300

    Base Mod

---
+++ src/client/java/io/github/lucasbq/projectx/ProjectXClient.java
@@ -0,0 +1,10 @@
+package io.github.lucasbq.projectx;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ProjectXClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/io/github/lucasbq/projectx/ProjectXDataGenerator.java
@@ -0,0 +1,11 @@
+package io.github.lucasbq.projectx;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ProjectXDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
